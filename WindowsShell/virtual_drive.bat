8d053767dd3d5d2db50877ce06af5234ddd81245
eslinux <luongduyninh@gmail.com>
2022-08-27 15:25:06 +0900

    update mount drive on windows

--- WindowsShell/virtual_drive.bat
+++ WindowsShell/virtual_drive.bat
@@ -1,13 +1,27 @@
 
-::Create virtual drive
-subst <drive:> \path\to\folder
+1. [subst] command
+	::Create virtual drive
+	subst <drive:> \path\to\folder
 
-Ex:
-subst E: C:\Users\user\workspace
+	Ex:
+	subst E: C:\Users\user\workspace
 
 
-::Remove Virtual Drive Created with Subst
-subst <drive:> /D
+	::Remove Virtual Drive Created with Subst
+	subst <drive:> /D
 
-Ex:
-subst N: /D
+	Ex:
+	subst N: /D
+	
+2. [Net use] command
+★https://lazyadmin.nl/it/net-use-command/
+
+	::Create virtual drive
+	Net use N: \\172.20.10.4\myshare /persistent:yes
+
+	# Authenticate with the username and password
+	Net use N: \\VBoxSvr\Win11\Documents /user:username password /p:yes
+	
+	::Remove Virtual Drive Created
+	Net use N: /delete
+	
