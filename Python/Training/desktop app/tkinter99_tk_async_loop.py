70974bc0dd4b18e646d351f64d833db99f2e1fc1
ninhld <luongduyninh@gmail.com>
2024-05-04 21:12:53 +0700

    update tkinter multithread

--- Python/Training/desktop app/tkinter99_tk_async_loop.py
+++ Python/Training/desktop app/tkinter99_tk_async_loop.py
@@ -1,83 +1,33 @@
-import asyncio
-from async_tkinter_loop import async_handler, async_mainloop
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from threading import Lock
+import time
+import threading
 
-async def btn_cancel():
-    global g_isCancel
-    g_isCancel = True
-    print("btn_cancel: ", g_isCancel)
-
-async def btn_click():
-    global g_isCancel
-    g_isCancel = False
+def btn_click():
     print("button clicked !")
-    b1.config(state="disable", text="Running ...")
-    while not g_isCancel:
-        print("btn_click: ", g_isCancel)
-        await asyncio.sleep(1.0)
+    lb.config(text="-:-")
+    threading.Thread(target=background_thread).start()
 
-    b1.config(state="normal", text="run")
+def background_thread():
+    print("start...")
+    for counter in range(0, 10):
+        time.sleep(1)
+        root.event_generate("<<event1>>", when="tail", state=counter)
+    print("end...")
+
+def event_handler(evt):  # runs in main thread
+    print(evt.state)  #data from event
+    lb.config(text=f"{evt.state}") #update UI here
 
 if __name__ == "__main__":
-    lock = Lock()
-    g_isCancel = False
-
-    # new approach
     root = ttk.Window(size=(720, 150))
-    # root.geometry("720x150") #or use this api
 
-    b1 = ttk.Button(root, text='run', bootstyle=PRIMARY, command=async_handler(btn_click))
-    b1.pack(padx=5, pady=5)
-    b2 = ttk.Button(root, text='cancel', bootstyle=SECONDARY, command=async_handler(btn_cancel))
-    b2.pack(padx=5, pady=5)
+    b1 = ttk.Button(root, text='primary', bootstyle=PRIMARY, command=btn_click)
+    b1.pack(side=LEFT, padx=5, pady=5)
 
-    async_mainloop(root)
-    
+    lb = ttk.Label(root, text="hihi")
+    lb.pack(side=LEFT, padx=5, pady=5)
 
-
-
-
-
-# # https://pypi.org/project/async-tkinter-loop/
-# import tkinter as tk
-# from io import BytesIO
-
-# import httpx
-# from PIL import Image, ImageTk
-
-# from async_tkinter_loop import async_handler, async_mainloop
-
-
-# async def load_image(url):
-#     button.config(state=tk.DISABLED)
-#     label.config(text="Loading...", image="")
-
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(url, follow_redirects=True)
-#         if response.status_code != 200:
-#             label.config(text=f"HTTP error {response.status_code}")
-#         else:
-#             content = response.content
-#             pil_image = Image.open(BytesIO(content))
-#             image = ImageTk.PhotoImage(pil_image)
-#             label.image = image
-#             label.config(image=image, text="")
-#             button.config(state=tk.NORMAL)
-
-
-# url = "https://picsum.photos/800/640"
-
-# root = tk.Tk()
-# root.geometry("800x640")
-
-# button = tk.Button(root, text="Load an image", command=async_handler(load_image, url))
-# button.pack()
-
-# label = tk.Label(root)
-# label.pack(expand=1, fill=tk.BOTH)
-
-# async_mainloop(root)
-
+    root.bind("<<event1>>", event_handler)  # event triggered by background thread
+    root.mainloop()
 
